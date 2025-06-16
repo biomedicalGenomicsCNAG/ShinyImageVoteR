2b8b28e40a4cdad3b697accc601262f576a01f6e
Ivo Leist <ivo.leist@googlemail.com>
2025-06-16 23:50:17 +0200

    added codex setup file R

--- codex_setup.sh
+++ codex_setup.sh
@@ -2,5 +2,4 @@
 # This script sets up the dev environment for the B1MG-variant-voting project in OpenAI codex.
 
 apt update && apt install r-base -y
-cd shiny
-R -e 'install.packages("pak", repos = "https://r-lib.github.io/p/pak/stable/source/linux-gnu/x86_64")'
+cd shiny && Rscript codex_setup.R
