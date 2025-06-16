30ababad0869661ffa97fdf938998f706b30aad3
Ivo Leist <ivo.leist@googlemail.com>
2025-06-17 00:04:39 +0200

    try to run R 4.5.0 in codex

--- codex_setup.sh
+++ codex_setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # This script sets up the dev environment for the B1MG-variant-voting project in OpenAI codex.
 
-apt update && apt install r-base -y
+curl -O https://cdn.posit.co/r/ubuntu-2404/pkgs/r-${R_VERSION}_1_$(dpkg --print-architecture).deb
+apt update
+apt install ./r-${R_VERSION}_1_$(dpkg --print-architecture).deb
+
 cd shiny && Rscript codex_setup.R
