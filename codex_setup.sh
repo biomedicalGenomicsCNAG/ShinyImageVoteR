b1ba2b0c78b701b7b54408f1e0a84843a6c8ab4e
Ivo Leist <ivo.leist@googlemail.com>
2025-06-17 00:35:37 +0200

    try to run R 4.5.0 in codex

--- codex_setup.sh
+++ codex_setup.sh
@@ -8,5 +8,3 @@ apt install ./r-${R_VERSION}_1_$(dpkg --print-architecture).deb -y
 
 ln -s /opt/R/${R_VERSION}/bin/R /usr/local/bin/R
 ln -s /opt/R/${R_VERSION}/bin/Rscript /usr/local/bin/Rscript
-
-cd shiny && Rscript codex_setup.R
--- shiny/codex_setup.R
+++ shiny/codex_setup.R
@@ -1,5 +1,2 @@
-install.packages("pak", repos = "https://r-lib.github.io/p/pak/stable/source/linux-gnu/x86_64")
-
-sys_release <- system("lsb_release -c -s", intern = TRUE)
-options(repos = c(CRAN = paste0("https://packagemanager.posit.co/cran/__linux__/", sys_release, "/latest")))
-source("renv/activate.R")
+library("renv")
+renv::restore()
