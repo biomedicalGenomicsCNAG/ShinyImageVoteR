ee3dfd80f1276b6501125f2adffc1d10fee03be6
Ivo Leist <ivo.leist@googlemail.com>
2025-06-16 23:57:10 +0200

    removed install renv with pak

--- shiny/codex_setup.R
+++ shiny/codex_setup.R
@@ -1,6 +1,4 @@
 install.packages("pak", repos = "https://r-lib.github.io/p/pak/stable/source/linux-gnu/x86_64")
-library(pak)
-pak("renv")
 
 sys_release <- system("lsb_release -c -s", intern = TRUE)
 options(repos = c(CRAN = paste0("https://packagemanager.posit.co/cran/__linux__/", sys_release, "/latest")))
