694e953b63197e80707922921c1c2d3022ba3a12
Ivo Leist <ivo.leist@googlemail.com>
2025-06-29 13:20:10 +0200

    Refactor voting module UI and made hotkeys functionality work with the voting module

--- shiny/ui.R
+++ shiny/ui.R
@@ -5,6 +5,7 @@ source("modules/user_stats_module.R")
 source("modules/about_module.R")
 
 source("modules/voting_module.R")
+
 color_seq <- function(seq, nt2color_map) {
   print("Coloring sequence:")
   print(seq)
@@ -35,37 +36,14 @@ render_voting_image_div <- function(mut_df, nt2color_map) {
   )
 }
 
-render_voting_questions_div <- function() {
-  div(
-    radioButtons(
-      inputId = "agreement",
-      label = cfg_radioBtns_label,
-      choices = cfg_radio_options2val_map
-    ),
-    conditionalPanel(
-      condition = "input.agreement == 'not_confident'",
-      checkboxGroupInput(
-        inputId = "observation",
-        label = cfg_checkboxes_label,
-        choices = cfg_observations2val_map
-      )
-    ),
-    conditionalPanel(
-      condition = "input.agreement == 'diff_var' || input.agreement == 'not_confident'",
-      textInput(
-        inputId = "comment",
-        label = "Comments",
-        value = ""
-      )
-    )
-  )
-}
-
 main_page <- function() {
    tagList(
     navbarPage(
       cfg_application_title,
-        votingUI("voting"),
+      tabPanel(
+        "Vote",
+        fluidPage(votingUI("voting"))
+      ),
       tabPanel(
         "Leaderboard",
         fluidPage(leaderboardUI("leaderboard"))
