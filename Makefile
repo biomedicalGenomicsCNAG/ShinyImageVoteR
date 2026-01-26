bea018926afc57ecbdf268751dcc4b43870d1d9b
Ivo Leist <ivo.leist@googlemail.com>
2026-01-26 16:54:51 +0100

    built newest version

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 
 # Package information
 PACKAGE_NAME = ShinyImgVoteR
-VERSION = 0.1.1
+VERSION = 0.1.2
 TARBALL = $(PACKAGE_NAME)_$(VERSION).tar.gz
 
 # R command
--- app.R
+++ app.R
@@ -1,5 +1,5 @@
 # script to run the app with shiny-server
-renv::install("ShinyImgVoteR_0.1.1.tar.gz")
+renv::install("ShinyImgVoteR_0.1.2.tar.gz")
 library(ShinyImgVoteR)
 
 # for shiny server deployment
@@ -7,7 +7,7 @@ host <- Sys.getenv("SHINY_HOST", "0.0.0.0")
 port <- as.integer(Sys.getenv("SHINY_PORT", 3838))
 
 ShinyImgVoteR::run_voting_app(
-    host = host,
-    port = port,
-    launch.browser = FALSE
+  host = host,
+  port = port,
+  launch.browser = FALSE
 )
